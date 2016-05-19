2fa4a54ecce3e824bafee29e6ac23c301fd4c6c4
aoki minami <aoki3331@gmail.com>
2016-05-19 13:57:26 +0900

    姓、名のテキストボックスを作成

---
+++ 02/contact.php
@@ -0,0 +1,15 @@
+<form action="result.php" method="post">
+
+<p>
+姓：<input type="textbox" name="name1" size="20">
+</p>
+
+<p>
+名：<input type="textbox" name="name2" size="20">
+</p>
+
+<p>
+<input type="submit">
+</p>
+
+</fomr>
---
+++ 02/result.php
@@ -0,0 +1,4 @@
+<?php
+echo $_POST['name1'] . " " . $_POST['name2'];
+//var_dump($_POST);
+?>
